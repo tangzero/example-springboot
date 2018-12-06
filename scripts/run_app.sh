9007d29e3005d4233c96a4407a1b27f8de4fbaec
luisherreratw <lherrera@thoughtworks.com>
2018-12-06 18:03:14 -0300

    adds multi-stages docker build

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# compile
-docker run --rm --net=todos -v $(pwd)/.:/opt/ -it -p 9090:8080 dockerdemo-base mvn -Dmaven.test.skip=true package
-# run
-docker run --rm --name=backend-java --net=todos --ip 172.18.0.21  -v $(pwd)/.:/opt/ -it -p 9090:8080 dockerdemo-base java -jar ./target/todo-0.0.1-SNAPSHOT.jar
+docker run --rm --name=backend-java \
+        --net=todos --ip 172.18.0.21 -it \
+        -p 9090:8080 dockerdemo-base
