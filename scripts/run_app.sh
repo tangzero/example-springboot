766ae84d463965e5ed2d019fa8c62991d027193c
luisherreratw <lherrera@thoughtworks.com>
2018-12-08 23:59:20 -0300

    adds jenkinsfile for docker containers

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-docker run --rm --name=backend-java \
-        --net=todos --ip 172.18.0.21 -it \
-        -p 9090:8080 herreraluis/javademo
+state_container=$(docker inspect -f {{.State.Running}} backend-java)
+if $state_container;
+then
+        docker rm -f backend-java
+fi
+
+docker run -d --name=backend-java \
+        --net=todos --ip 172.18.0.21 \
+        --restart=always \
+        -p 9090:8080 herreraluis/javademo 
