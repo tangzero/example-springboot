c0f859ac21dcb63072f96378e4191a9fb87c9d53
luisherreratw <lherrera@thoughtworks.com>
2018-12-09 01:00:14 -0300

    adds version parameter to image for deploy container

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mayor_version="1.0"
+minor_version=$BUILD_NUMBER
+version="$mayor_version.$minor_version"
+
 state_container=$(docker inspect -f {{.State.Running}} backend-java)
 if $state_container;
 then
@@ -9,4 +13,4 @@ fi
 docker run -d --name=backend-java \
         --net=todos --ip 172.18.0.21 \
         --restart=always \
-        -p 9090:8080 herreraluis/javademo 
+        -p 9090:8080 herreraluis/javademo:$version 
